377f20aeb891a721f15902983e635d404c57d093
okadan <naoki.okada@beproud.jp>
2016-12-12 19:22:48 +0900

    support stopLoading

--- ios/RCTWKWebView/RCTWKWebView.h
+++ ios/RCTWKWebView/RCTWKWebView.h
@@ -31,6 +31,7 @@ shouldStartLoadForRequest:(NSMutableDictionary<NSString *, id> *)request
 - (void)goForward;
 - (void)goBack;
 - (void)reload;
+- (void)stopLoading;
 - (void)evaluateJavaScript:(NSString *)javaScriptString completionHandler:(void (^)(id, NSError *error))completionHandler;
 
 @end
