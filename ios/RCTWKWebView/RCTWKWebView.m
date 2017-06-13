7199f9a886ff20aa007fe5a29c4506657c7583c3
JUNG LAE JO <junglaejo@JUNGs-Mac-mini.local>
2017-06-13 20:52:08 +0900

    RFC 6265, section 4.1: Set-Cookie;

--- ios/RCTWKWebView/RCTWKWebView.m
+++ ios/RCTWKWebView/RCTWKWebView.m
@@ -321,6 +321,22 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
   }
 }
 
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler
+{
+    if (_sendCookies)
+    {
+      NSArray *cookies = [NSHTTPCookie cookiesWithResponseHeaderFields:((NSHTTPURLResponse *)navigationResponse.response).allHeaderFields forURL:((NSHTTPURLResponse *)navigationResponse.response).URL];
+      
+      for (int i = 0; i < cookies.count; i++) {
+        NSHTTPCookie *cookie = [cookies objectAtIndex:i];
+        NSLog(@"cookie: name=%@, value=%@", cookie.name, cookie.value);
+        [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+      }
+    }
+  
+  decisionHandler(WKNavigationResponsePolicyAllow);
+}
+
 - (void)webView:(__unused WKWebView *)webView didFailProvisionalNavigation:(__unused WKNavigation *)navigation withError:(NSError *)error
 {
   if (_onLoadingError) {
