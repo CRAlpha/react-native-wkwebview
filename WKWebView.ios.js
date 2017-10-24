9d80ae7b8cb888881a87d501564b674c32463628
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-10-24 14:33:01 -0300

    Adding support for react 16

--- WKWebView.ios.js
+++ WKWebView.ios.js
@@ -1,8 +1,9 @@
 'use strict';
 
-import React, {
-  PropTypes
-} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
+
 import ReactNative, {
   requireNativeComponent,
   EdgeInsetsPropType,
@@ -72,7 +73,7 @@ var defaultRenderError = (errorDomain, errorCode, errorDesc) => (
  * Renders a native WebView.
  */
 
-var WKWebView = React.createClass({
+var WKWebView = createReactClass({
   statics: {
     JSNavigationScheme: JSNavigationScheme,
     NavigationType: NavigationType,
@@ -266,7 +267,7 @@ var WKWebView = React.createClass({
     });
 
     if (this.props.source && typeof this.props.source == 'object') {
-      var source = Object.assign({}, this.props.source, { 
+      var source = Object.assign({}, this.props.source, {
         sendCookies: this.props.sendCookies,
         customUserAgent: this.props.customUserAgent || this.props.userAgent
       });
