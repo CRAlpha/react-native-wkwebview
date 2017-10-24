a597f9d40efb4121ebede84e71c708987dc789f2
Felipe Torres <felipe.torressepulveda@gmail.com>
2017-10-24 14:34:15 -0300

    removing_space

--- WKWebView.ios.js
+++ WKWebView.ios.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
-
 import ReactNative, {
   requireNativeComponent,
   EdgeInsetsPropType,
