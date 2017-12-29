19b04e81ca61b75487e7b450c8316518df7b6ef5
森田 昌宏 <m.morita@getgamba.com>
2017-12-29 12:58:30 +0900

    make scrollView not lift up when keyboard appears with scrollEnabled is false

--- ios/RCTWKWebView/RCTWKWebView.m
+++ ios/RCTWKWebView/RCTWKWebView.m
@@ -68,10 +68,16 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
     _webView = [[WKWebView alloc] initWithFrame:self.bounds configuration:config];
     _webView.UIDelegate = self;
     _webView.navigationDelegate = self;
+    _webView.scrollView.delegate = self;
     [_webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
     [self addSubview:_webView];
   }
   return self;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+  if (!scrollView.scrollEnabled)
+    scrollView.bounds = _webView.bounds;
 }
 
 - (void)loadRequest:(NSURLRequest *)request
