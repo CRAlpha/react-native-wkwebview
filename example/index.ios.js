94aac3337b44d86ac4491948b165517e37c778ad
Ruoyu <ruoysun@gmail.com>
2018-02-12 10:06:11 +0200

    Fix build error

--- example/index.ios.js
+++ example/index.ios.js
@@ -9,18 +9,18 @@ import WkWebView from 'react-native-wkwebview-reborn';
 
 export default class example extends Component {
   componentWillMount() {
-    
+
   }
   render() {
     return (
       <View style={{ flex: 1, marginTop: 20 }}>
-        <WkWebView style={{ backgroundColor: '#ff0000' }} 
+        <WkWebView style={{ backgroundColor: '#ff0000' }}
           userAgent="MyFancyWebView"
           hideKeyboardAccessoryView={false}
-          ref="webview" 
-          sendCookies={true} 
+          ref={c => this._webview = c}
+          sendCookies={true}
           source={{ uri: 'https://httpbin.org/get' }} />
-        <Text style={{ fontWeight: 'bold', padding: 10 }} onPress={() => this.refs.webview.reload()}>Reload</Text>
+        <Text style={{ fontWeight: 'bold', padding: 10 }} onPress={() => this._webview.reload()}>Reload</Text>
       </View>
     );
   }
