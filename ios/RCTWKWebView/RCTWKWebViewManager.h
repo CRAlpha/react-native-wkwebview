36e56c7cfa3c4abd1fc239eaad2b2ddea97cabc8
noamco <noamc@wix.com>
2018-06-18 14:53:50 +0300

    change webview to be a shared instance (experimental, does not support back navigation)

--- ios/RCTWKWebView/RCTWKWebViewManager.h
+++ ios/RCTWKWebView/RCTWKWebViewManager.h
@@ -1,10 +1,11 @@
 #import <React/RCTViewManager.h>
 #import <React/RCTConvert.h>
+#import "RCTWKWebView.h"
 
 @interface RCTConvert (UIScrollView)
 
 @end
 
 @interface RCTWKWebViewManager : RCTViewManager
-
+@property (nonatomic, strong) RCTWKWebView * webView;
 @end
